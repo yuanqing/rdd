3a7a352983e1e7ec573427afe6c281e9687a27fe
yuanqing <desk@yuanqing.sg>
2015-05-27 05:01:41 +0800

    Add tests

--- server.js
+++ server.js
@@ -2,11 +2,11 @@
 
 var ecstatic = require('ecstatic');
 var fs = require('fs');
-var gaze = require('gaze');
+var Gaze = require('gaze');
+var hljs = require('highlight.js');
 var http = require('http');
 var Remarkable = require('remarkable');
 var shoe = require('shoe');
-var hljs = require('highlight.js');
 
 var md = new Remarkable('full', {
   html: true,
@@ -29,25 +29,32 @@ var serve = function(file, port, cb) {
   var server = http.createServer(ecstatic(__dirname));
   server.listen(port);
 
+  var watcher;
+
+  var watch = function(stream) {
+    var gaze = new Gaze(file);
+    gaze.on('changed', function() {
+      render(file, stream);
+    });
+    gaze.on('deleted', function() {
+      close();
+      cb(file + ': No such file');
+    });
+    return gaze;
+  };
+
+  var close = function() {
+    server.close();
+    watcher.close();
+  };
+
   var sock = shoe(function(stream) {
     render(file, stream);
-    gaze(file, function(err, watcher) {
-      if (err) {
-        return cb(err);
-      }
-      watcher.on('changed', function() {
-        render(file, stream);
-      });
-      watcher.on('deleted', function() {
-        server.close();
-        watcher.close();
-        cb(file + ': No such file');
-      });
-    });
+    watcher = watch(stream);
   });
   sock.install(server, '/rdd');
 
-  return server;
+  return close;
 
 };
 
