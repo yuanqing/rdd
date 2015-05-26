e13855559bd4f7399be8d15d71254d986c52aaf1
yuanqing <desk@yuanqing.sg>
2015-05-27 00:37:28 +0800

    Exit if README was deleted

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ var render = function(file, stream) {
   stream.write(md.render(fs.readFileSync(file, 'utf8')));
 };
 
-var serve = function(file, port) {
+var serve = function(file, port, cb) {
 
   var server = http.createServer(ecstatic(__dirname));
   server.listen(port);
@@ -23,12 +23,22 @@ var serve = function(file, port) {
   var sock = shoe(function(stream) {
     render(file, stream);
     gaze(file, function(err, watcher) {
-      watcher.on('all', function(event, file) {
+      if (err) {
+        return cb(err);
+      }
+      watcher.on('changed', function() {
         render(file, stream);
+      });
+      watcher.on('deleted', function() {
+        server.close();
+        watcher.close();
+        cb(file + ': No such file');
       });
     });
   });
   sock.install(server, '/rdd');
+
+  return server;
 
 };
 
