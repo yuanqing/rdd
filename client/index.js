afdccb1498fcad01a0821c63885023b7bf209b52
Yuan Qing <desk@yuanqing.sg>
2018-02-05 23:35:09 +0800

    Rename

--- client/index.js
+++ client/index.js
@@ -5,11 +5,11 @@ var element = document.querySelector('.markdown-body')
 var path = window.location.pathname.substring(1)
 var port = parseInt(window.location.port) + 1
 
-var ws = new WebSocket('ws://localhost:' + port + '/')
-ws.onopen = function () {
-  ws.send(path)
+var webSocket = new WebSocket('ws://localhost:' + port + '/')
+webSocket.onopen = function () {
+  webSocket.send(path)
 }
-ws.onmessage = function (event) {
+webSocket.onmessage = function (event) {
   morphdom(
     element,
     '<article class="markdown-body">' + event.data + '</article>'
