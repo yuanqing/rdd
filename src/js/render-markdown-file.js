63c8919ffeb56cba11a787d9b9ac411197c1dfe5
yuanqing <desk@yuanqing.sg>
2019-07-04 02:21:36 +0800

    Add `markdown-toc`

--- src/js/render-markdown-file.js
+++ src/js/render-markdown-file.js
@@ -3,6 +3,7 @@ const MarkdownIt = require('markdown-it')
 const markdownItEmoji = require('markdown-it-emoji')
 const markdownItGithubHeadings = require('markdown-it-github-headings')
 const markdownItHighlightJs = require('markdown-it-highlightjs')
+const markdownToc = require('markdown-toc')
 
 const m = new MarkdownIt({
   html: true,
@@ -12,9 +13,15 @@ const m = new MarkdownIt({
   .use(markdownItGithubHeadings)
   .use(markdownItHighlightJs)
 
+function insertToc (markdown) {
+  return markdownToc.insert(markdown, {
+    bullets: '-'
+  })
+}
+
 async function renderMarkdownFile (file) {
   const markdown = await fs.readFile(file, 'utf8')
-  return m.render(markdown)
+  return m.render(insertToc(markdown))
 }
 
 module.exports = renderMarkdownFile
